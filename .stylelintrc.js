41b53b11bd5ac0156aee6188c56bc6f2c6f7e213
shio <ag2.cry@gmail.com>
2024-01-27 19:51:05 +0900

    add setup file

---
+++ .stylelintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ['stylelint-config-standard', 'stylelint-config-recess-order'],
+  plugins: ['stylelint-order'],
+  ignoreFiles: ['**/node_modules/**'],
+  rules: {
+    'string-quotes': 'single',
+    'font-family-name-quotes': null,
+    'selector-class-pattern': null,
+  },
+};
